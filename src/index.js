e8f1221e7fd45d9d4e5581e28711e515eae00fc3
Denys Dovhan <denysdovhan@gmail.com>
2018-02-15 20:56:13 +0200

    Clean-up project

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
